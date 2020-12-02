e6685da5cb98b0f05d7b6cc89ebcc6d784700198
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-12-03 00:05:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ printf '\033[32m;echo script by thelearn-tech
 sleep 1
 echo installing
 pkg install proot
-cd $PREFIX/bin
-touch root
-echo "proot -0" >> root
+mv root $PREFIX/bin
 chmod +x root
 sleep 2
 clear
 echo installed
 clear
+cd
 echo type "root" for fake root
+rm -rf fake-root
 exit 1
